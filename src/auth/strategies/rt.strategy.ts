748e35da7f52bb3b468281dc266bef8a9e567879
RafaelPil <rafael.pilumyan@gmail.com>
2024-06-22 18:43:21 +0300

    NestJs Jwt Strategy

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,12 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'at-secret',
+    });
+  }
+}
